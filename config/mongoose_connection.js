def07ddc8c7d0371555715a6f637246d050de2dc
Ritovan <ritovan0302@gmail.com>
2025-02-23 11:59:04 +0530

    added debugging and creater owner routing

--- config/mongoose_connection.js
+++ config/mongoose_connection.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const dbgr = require('debug')('development:mongoose');
+require("dotenv").config();
+const uri = process.env.MONGO_URI;
 // Connect to MongoDB
 
 mongoose
-.connect('mongodb://localhost:27017/scatch')
-.then(() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to MongoDB...', err));
+.connect(`${uri}/scatch`)
+.then(() => dbgr('Connected to MongoDB...'))
+.catch(err => dbgr('Could not connect to MongoDB...', err));
 
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner_model');
 
 router.get('/', (req, res) => {
   res.send("404: Page not found");
+});
+
+router.post('/create', async (req, res) => {
+  let owners = await ownerModel.find();
+  if(owners.length > 0){
+    return res.send("Owner already exists").sendStatus(503);
+  }
+  let newOwner = new ownerModel({
+    fullname: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+  await newOwner.save();
+  res.send(newOwner).status(201);
+  
+
 });
 
 module.exports = router;
